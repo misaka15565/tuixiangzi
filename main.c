92ab20e4a8df8408acf6cbd3326b44b8d7450f77
misaka15565 <hhj@11ban.top>
2023-02-16 18:59:36 +0800

    gui示例

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    printf("墙墙墙墙墙墙墙墙墙墙\n");
+    printf("墙                墙\n");
+    printf("墙                墙\n");
+    printf("墙                墙\n");
+    printf("墙                墙\n");
+    printf("墙                墙\n");
+    printf("墙                墙\n");
+    printf("墙                墙\n");
+    printf("墙                墙\n");
+    printf("墙墙墙墙墙墙墙墙墙墙\n");
+}
